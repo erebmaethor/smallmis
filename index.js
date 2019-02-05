da806c136964e8fe3194f9e56a9363d30781c2ce
Mikhail Smolin <erebmaethor@gmail.com>
2019-02-05 23:21:46 +0300

    Some reorganising of project structure

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const startHapiServer = require('./server');
+const server = require('./server/hapi');
+const mongoose = require('mongoose');
+require('./server/mongo');
 
-startHapiServer();
+(async () => {
+
+  try {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+    
+  } catch(e) {
+
+    console.error(e);
+  }
+
+})();
+
+const gracefulExit = () => { 
+  mongoose.connection.close(() => {
+    process.exit(0);
+  });
+}
+
+process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
